df0a7a4c1c94e3d2edc2a474b0d97af403f5f7f1
Jeslinthomaskutty <jeslinthomaskutty21@gmail.com>
2025-03-11 18:50:17 +0530

    Fix formatting by adding a trailing newline at the end of app.py

--- app.py
+++ app.py
@@ -240,4 +240,4 @@ def video_feed():
     return Response(gen_frames(video_path), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
